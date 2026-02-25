97fd4ed22af8b4423f8f3f67a8ec3263bd48a009
jyotirajput-dev <jyotirajput.ai67@gmail.com>
2026-02-25 18:49:43 +0530

    Add files via upload

---
+++ 1hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+this is my first C program 
+which is awesome 
+*/
+int main(){
+    int _tom;
+    int Tom;
+    // Declaring variables to store error codes 
+    int error_code;
+    //'A'--> a character
+    printf("hello jyoti welcome back to your learning \n");
+    printf("you can do it just believe in yourself\n");
+
+    return 0;
+}
