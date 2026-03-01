7ce2cabb4bd8e6ee5055f88de7860125b9782ddf
jyotirajput-dev <jyotirajput.ai67@gmail.com>
2026-03-01 12:11:47 +0530

    Add files via upload

---
+++ 1hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+This is my first C program
+*/
+int main()
+{
+    int _cat; // we can only use underscore cann't use othe signs like @ # $ % it will throw error
+    int Cat;  // case sensitive cat and Cat are two different variables both have their own memory allocated
+    // Declaring variables to store error codes
+    int error_code; // for readability use meaningful names acc. to your project so in future you won't forget
+    //'A'--> a character
+    printf("hello jyoti welcome back to your learning \n");
+    printf("you can do it just believe in yourself\n");
+    // printf is used through the because we installed #include <stdio.h> ki library ke andar inko already define kiya hai
+    // isliye hum inhe use kar directly print karte hai yeh inbuilt hai
+    return 0;
+}
