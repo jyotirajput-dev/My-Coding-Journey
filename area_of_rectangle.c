48e7228f61d65080ef7d3dcdf8034f0268ffdc3f
Jy <jyotirajput.ai67@gmail.com>
2026-03-06 06:57:44 +0530

    Add files via upload

---
+++ area_of_rectangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+    Program: Area of Rectangle
+    Description:
+    This program calculates the area of a rectangle
+    using the formula: Area = Length × Breadth
+*/
+
+int main()
+{
+    int length = 98;
+    int breadth = 65;
+
+    int area = length * breadth;
+
+    printf("The area of the rectangle is %d\n", area);
+
+    return 0;
+}
